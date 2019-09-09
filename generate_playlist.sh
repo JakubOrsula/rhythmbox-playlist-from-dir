ed7a172f0f84ca2f1aa1297a13d5fa457f88f5b8
JakubOrsula <36151189+JakubOrsula@users.noreply.github.com>
2019-09-09 19:28:27 +0200

    proper directory names encoding

--- generate_playlist.sh
+++ generate_playlist.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 ## generate rhythmbox playlist from folders in directory
+
+## url encode taken from pure bash bible
+urlencode() {
+    # Usage: urlencode "string"
+    local LC_ALL=C
+    for (( i = 0; i < ${#1}; i++ )); do
+        : "${1:i:1}"
+        case "$_" in
+            [a-zA-Z0-9.~_-])
+                printf '%s' "$_"
+            ;;
+
+            *)
+                printf '%%%02X' "'$_"
+            ;;
+        esac
+    done
+    printf '\n'
+}
+
+
 fout="rhythmbox_partial_playlist.xml"
 echo -n "" > $fout;
 exec >> $fout;
 for dir in *; do
         if [[ -d "$dir" ]]; then
-		dir_escaped=${dir// /%20};
+		dir_escaped=$(urlencode_safe='/' urlencode "$dir")
                 printf "<playlist name=\"%s\" show-browser=\"true\" browser-position=\"180\" search-type=\"search-match\" type=\"automatic\" sort-key=\"Artist\" sort-direction=\"0\">\n" "$dir";
                 echo "<conjunction>";
                 printf "<equals prop=\"type\">song</equals>\n";
